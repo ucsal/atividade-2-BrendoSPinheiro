75b424cbfa146a6407f20a3c925e87ef0924afd2
Brendo <brendo.spinheiro@gmail.com>
2020-04-17 15:15:37 -0300

    Template 4 testes aprovados

--- app/src/main/java/br/com/mariojp/exercise2/MainActivity.java
+++ app/src/main/java/br/com/mariojp/exercise2/MainActivity.java
@@ -1,14 +1,48 @@
 package br.com.mariojp.exercise2;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Button btnTrocarUsuario;
+    private String usuario = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        btnTrocarUsuario = findViewById(R.id.btnTrocar);
+
+        btnTrocarUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(MainActivity.this, OutraActivity.class);
+                in.putExtra("USUARIO", usuario);
+                startActivityForResult(in, 200);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(data.hasExtra("USUARIO")){
+            if(data.getStringExtra("USUARIO").trim().isEmpty()){
+                usuario="";
+                textView.setText("Oi!");
+            }else{
+                usuario=data.getStringExtra("USUARIO");
+                textView.setText("Oi, "+usuario+"!");
+            }
+        }
     }
 }
---
+++ app/src/main/java/br/com/mariojp/exercise2/OutraActivity.java
@@ -0,0 +1,45 @@
+package br.com.mariojp.exercise2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class OutraActivity extends AppCompatActivity {
+
+    private Button btnConfirmar;
+    private Button btnCancelar;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_outra);
+        btnConfirmar = findViewById(R.id.btnConfirmar);
+        btnCancelar = findViewById(R.id.btnCancelar);
+        editText = findViewById(R.id.editText);
+
+        if(getIntent().hasExtra("USUARIO")){
+            editText.setText((getIntent().getStringExtra("USUARIO")));
+        }
+
+        btnConfirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getIntent().putExtra("USUARIO", editText.getText().toString());
+                setResult(200, getIntent());
+                finish();
+            }
+        });
+
+        btnCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(500, null);
+                finish();
+            }
+        });
+    }
+}
